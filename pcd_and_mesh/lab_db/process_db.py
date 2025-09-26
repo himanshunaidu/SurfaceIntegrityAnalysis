f385e286581a4df95cba72589176aa16a41d7f8a
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-09-26 16:56:47 -0700

    Verify 3d object extraction and align point cloud with mesh

--- pcd_and_mesh/lab_db/process_db.py
+++ pcd_and_mesh/lab_db/process_db.py
@@ -15,6 +15,20 @@ import open3d as o3d
 NOTE: The point cloud is read from a dedicated point cloud directory.
 Other data (RGB, mesh) is read from the main trial directory.
 """
+TRANSFORM_COLUMNS_TO_MATRIX_MAP = {
+    "rxx": (0, 0),
+    "rxy": (1, 0),
+    "rxz": (2, 0),
+    "ryx": (0, 1),
+    "ryy": (1, 1),
+    "ryz": (2, 1),
+    "rzx": (0, 2),
+    "rzy": (1, 2),
+    "rzz": (2, 2),
+    "x": (0, 3),
+    "y": (1, 3),
+    "z": (2, 3)
+}
 
 def read_pcd(dir_path: str, row_trial_name: str) -> o3d.geometry.PointCloud:
     """
@@ -29,6 +43,72 @@ def read_pcd(dir_path: str, row_trial_name: str) -> o3d.geometry.PointCloud:
 
     print(f"Loaded point cloud from {os.path.join(dir_path, f'{row_trial_name}.ply')} with {len(pcd.points)} points for trial '{row_trial_name}'")
     return pcd
+
+def align_pcd(pcd: o3d.geometry.PointCloud, transform: np.ndarray) -> tuple[o3d.geometry.PointCloud, np.ndarray]:
+    """
+    Align the point cloud to the camera coordinate system using the given transformation matrix.
+    """
+    if pcd.is_empty():
+        raise ValueError("Input point cloud is empty.")
+    
+    if transform.shape != (4, 4):
+        raise ValueError(f"Transform matrix must be 4x4, got shape {transform.shape}")
+    
+    pcd.estimate_normals()
+    pcd.orient_normals_towards_camera_location()
+    
+    # Remove translation component for alignment
+    adjusted_transform = transform.copy()
+    adjusted_transform[:3, 3] = 0
+    print(f"Initial transform (no translation):\n{adjusted_transform}")
+
+    # Invert the y-axis and z-axis to convert from ARKit to Open3D coordinate system.
+    # convert_matrix = np.diag([1, -1, -1, 1])
+    # adjusted_transform = convert_matrix @ transform @ convert_matrix
+    
+    # The ARKit camera transform was calculated in portrait mode, so we need to rotate the point cloud accordingly.
+    # This rotation is 90 degrees around the z-axis, with x changing to -y and y changing to x.
+    rotation_90_z = np.array(
+        [[0, 1, 0, 0],
+         [-1, 0, 0, 0],
+         [0, 0, 1, 0],
+         [0, 0, 0, 1]]
+    )
+    adjusted_transform = adjusted_transform @ np.linalg.inv(rotation_90_z)
+    print(f"Applying adjusted transform to point cloud:\n{adjusted_transform}")
+    
+    Y = np.array([0, 1, 0])
+    
+    # Orient the transform's x-axis to point parallel to the ground plane.
+    # Thus, its x and z components remain the same, but y component is adjusted to be 0.
+    # Then, orient the transform's y-axis to point upwards (opposite to gravity).
+    # Thus, rotate the transform around x-axis by pitch = - current_pitch
+    rotation = adjusted_transform[:3, :3]
+    x, y, z = rotation[:, 0], rotation[:, 1], rotation[:, 2]
+    x_new = np.array([x[0], 0., x[2]])
+    x_new /= np.linalg.norm(x_new)
+    z_new = np.cross(x_new, Y)
+    z_new /= np.linalg.norm(z_new)
+    y_new = np.cross(z_new, x_new)
+    y_new /= np.linalg.norm(y_new)
+    
+    adjusted_transform[:3, 0] = x_new
+    adjusted_transform[:3, 1] = y_new
+    adjusted_transform[:3, 2] = z_new
+    print(f"Final transform after aligning x-axis to ground plane and y-axis to gravity:\n{adjusted_transform}")
+
+    pcd_homogeneous = np.hstack((np.asarray(pcd.points), np.ones((len(pcd.points), 1))))
+    aligned_points_homogeneous = (adjusted_transform @ pcd_homogeneous.T).T
+    aligned_points = aligned_points_homogeneous[:, :3]
+
+    aligned_pcd = o3d.geometry.PointCloud()
+    aligned_pcd.points = o3d.utility.Vector3dVector(aligned_points)
+    if pcd.has_colors():
+        aligned_pcd.colors = pcd.colors
+    if pcd.has_normals():
+        aligned_pcd.normals = pcd.normals
+
+    return aligned_pcd, adjusted_transform
 
 def read_rgb(dir_path: str, row_trial_name: str) -> Image.Image:
     """
@@ -67,7 +147,7 @@ def read_mesh(dir_path: str, row_trial_name: str) -> o3d.geometry.TriangleMesh:
     print(f"Loaded mesh from {mesh_path} with {len(mesh.triangles)} triangles for trial '{row_trial_name}'")
     return mesh
 
-def read_transform(dir_path: str, row_trial_name: str) -> pd.DataFrame:
+def read_transform(dir_path: str, row_trial_name: str) -> np.ndarray:
     """
     Reads the camera-to-world transformation matrix from a text file.
     """
@@ -78,13 +158,23 @@ def read_transform(dir_path: str, row_trial_name: str) -> pd.DataFrame:
     if not os.path.exists(transform_path):
         raise FileNotFoundError(f"Transform file not found: {transform_path}")
     
-    transform = pd.read_csv(transform_path, header=None)
+    transform_df = pd.read_csv(transform_path, delimiter=', ')
     # Will later convert to required format.
+    transform_row = transform_df.iloc[0].to_dict()
+    transform = np.eye(4)
+    for col, (i, j) in TRANSFORM_COLUMNS_TO_MATRIX_MAP.items():
+        if col not in transform_row:
+            raise ValueError(f"Column '{col}' not found in transform file: {transform_path}")
+        transform[i, j] = transform_row[col]
+        
+    # Invert the y-axis and z-axis to convert from ARKit to Open3D coordinate system.
+    # convert_matrix = np.diag([1, -1, -1, 1])
+    # transform = convert_matrix @ transform @ convert_matrix
 
     print(f"Loaded transform matrix from {transform_path} for trial '{row_trial_name}':\n{transform}")
     return transform
 
-def read_intrinsics(dir_path: str, row_trial_name: str) -> pd.DataFrame:
+def read_intrinsics(dir_path: str, row_trial_name: str) -> np.ndarray:
     """
     Reads the camera intrinsics matrix from a text file.
     """
@@ -95,8 +185,8 @@ def read_intrinsics(dir_path: str, row_trial_name: str) -> pd.DataFrame:
     if not os.path.exists(intrinsics_path):
         raise FileNotFoundError(f"Intrinsics file not found: {intrinsics_path}")
     
-    intrinsics = pd.read_csv(intrinsics_path, header=None)
-    # Will later convert to required format.
+    # intrinsics = pd.read_csv(intrinsics_path, header=None)
+    intrinsics = np.loadtxt(intrinsics_path, delimiter=',')
 
     print(f"Loaded intrinsics matrix from {intrinsics_path} for trial '{row_trial_name}':\n{intrinsics}")
     return intrinsics
@@ -118,3 +208,19 @@ def read_main_data(dir_path: str, row_trial_name: str) -> tuple[Image.Image, o3d
     intrinsics = read_intrinsics(row_path, row_trial_name)
 
     return img, mesh, transform, intrinsics
+
+if __name__=="__main__":
+    DATASET_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "dataset", "lab_controlled", "experiment_4"))
+    ROW_LABEL = "0-0-0-0-b"
+    
+    MAIN_PATH = os.path.join(DATASET_PATH, "main")
+    PCD_PATH = os.path.join(DATASET_PATH, "pcd")
+    
+    img, mesh, transform, intrinsics = read_main_data(MAIN_PATH, ROW_LABEL)
+    pcd = read_pcd(PCD_PATH, ROW_LABEL)
+    pcd, adjusted_transform = align_pcd(pcd, transform)
+    
+    frame_mesh = o3d.geometry.TriangleMesh.create_coordinate_frame().scale(0.1, np.zeros(3))
+    reference_frame_mesh = o3d.geometry.TriangleMesh.create_coordinate_frame().scale(0.2, np.zeros(3))
+
+    o3d.visualization.draw_geometries([pcd, mesh, frame_mesh.transform(adjusted_transform), reference_frame_mesh])

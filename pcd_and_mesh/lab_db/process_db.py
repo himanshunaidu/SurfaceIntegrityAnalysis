3f58dd170aad47bfc0cf2869d5c547aff2d1fc6e
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-09-27 23:08:11 -0700

    Modularize lab_db process script and add april tags detectors

--- pcd_and_mesh/lab_db/process_db.py
+++ pcd_and_mesh/lab_db/process_db.py
@@ -5,44 +5,27 @@ The data includes RGB and mesh data from SurfaceIntegrityChecker iOS app, and po
 import os
 import sys
 import glob
+
 import numpy as np
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 import cv2
 import open3d as o3d
 
+from utils.read_utils import read_pcd, read_rgb, read_mesh, read_transform, read_intrinsics, read_main_data
+
 """
 NOTE: The point cloud is read from a dedicated point cloud directory.
 Other data (RGB, mesh) is read from the main trial directory.
 """
-TRANSFORM_COLUMNS_TO_MATRIX_MAP = {
-    "rxx": (0, 0),
-    "rxy": (1, 0),
-    "rxz": (2, 0),
-    "ryx": (0, 1),
-    "ryy": (1, 1),
-    "ryz": (2, 1),
-    "rzx": (0, 2),
-    "rzy": (1, 2),
-    "rzz": (2, 2),
-    "x": (0, 3),
-    "y": (1, 3),
-    "z": (2, 3)
+
+APRIL_TAGS_USED = {
+    "BL": 201,
+    "BR": 202,
+    "M": 102, # Middle
+    "TL": 203,
+    "TR": 204
 }
-
-def read_pcd(dir_path: str, row_trial_name: str) -> o3d.geometry.PointCloud:
-    """
-    Read a point cloud file (PCD format) and return an Open3D PointCloud object.
-    """
-    if not os.path.exists(dir_path):
-        raise FileNotFoundError(f"PCD dir not found: {dir_path}")
-
-    pcd = o3d.io.read_point_cloud(os.path.join(dir_path, f"{row_trial_name}.ply"))
-    if pcd.is_empty():
-        raise ValueError(f"Loaded point cloud is empty: {row_trial_name}")
-
-    print(f"Loaded point cloud from {os.path.join(dir_path, f'{row_trial_name}.ply')} with {len(pcd.points)} points for trial '{row_trial_name}'")
-    return pcd
 
 def align_pcd(pcd: o3d.geometry.PointCloud, transform: np.ndarray) -> tuple[o3d.geometry.PointCloud, np.ndarray]:
     """
@@ -110,117 +93,65 @@ def align_pcd(pcd: o3d.geometry.PointCloud, transform: np.ndarray) -> tuple[o3d.
 
     return aligned_pcd, adjusted_transform
 
-def read_rgb(dir_path: str, row_trial_name: str) -> Image.Image:
-    """
-    Reads the RGB image from the given directory.
-    """
-    if not os.path.exists(dir_path):
-        raise FileNotFoundError(f"Main dir not found: {dir_path}")
+def detect_apriltags(img: Image.Image, aruco_detector=None) -> tuple[np.ndarray, np.ndarray]:
+    if aruco_detector is None:
+        dic = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_36h11)
+        parameters = cv2.aruco.DetectorParameters()
+        aruco_detector = cv2.aruco.ArucoDetector(dic, parameters)
 
-    # Currently, the rgb image can have any name, but it is placed alone in a folder as a png image.
-    img_files = glob.glob(os.path.join(dir_path, "*.png"))
-    if not img_files:
-        raise FileNotFoundError(f"No PNG image found in dir: {dir_path}")
-    img_path = img_files[0]
-
-    img = Image.open(img_path)
-    print(f"Loaded RGB image from {img_path} with size {img.size} for trial '{row_trial_name}'")
-    return img
-
-def read_mesh(dir_path: str, row_trial_name: str) -> o3d.geometry.TriangleMesh:
-    """
-    Reads the mesh file (PLY format) from the given directory.
-    """
-    if not os.path.exists(dir_path):
-        raise FileNotFoundError(f"Main dir not found: {dir_path}")
-
-    # Currently, the mesh file can have any name, but it is placed alone in a folder as a ply file.
-    mesh_files = glob.glob(os.path.join(dir_path, "*.ply"))
-    if not mesh_files:
-        raise FileNotFoundError(f"No PLY mesh file found in dir: {dir_path}")
-    mesh_path = mesh_files[0]
-
-    mesh = o3d.io.read_triangle_mesh(mesh_path)
-    if mesh.is_empty():
-        raise ValueError(f"Loaded mesh is empty: {mesh_path}")
-
-    print(f"Loaded mesh from {mesh_path} with {len(mesh.triangles)} triangles for trial '{row_trial_name}'")
-    return mesh
-
-def read_transform(dir_path: str, row_trial_name: str) -> np.ndarray:
-    """
-    Reads the camera-to-world transformation matrix from a text file.
-    """
-    if not os.path.exists(dir_path):
-        raise FileNotFoundError(f"Main dir not found: {dir_path}")
-
-    transform_path = os.path.join(dir_path, f"camera_transform.csv")
-    if not os.path.exists(transform_path):
-        raise FileNotFoundError(f"Transform file not found: {transform_path}")
+    img_array = np.array(img)
+    image_cv2 = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
+    gray = cv2.cvtColor(image_cv2, cv2.COLOR_BGR2GRAY)
+    corners, ids, rejected = aruco_detector.detectMarkers(gray)
     
-    transform_df = pd.read_csv(transform_path, delimiter=', ')
-    # Will later convert to required format.
-    transform_row = transform_df.iloc[0].to_dict()
-    transform = np.eye(4)
-    for col, (i, j) in TRANSFORM_COLUMNS_TO_MATRIX_MAP.items():
-        if col not in transform_row:
-            raise ValueError(f"Column '{col}' not found in transform file: {transform_path}")
-        transform[i, j] = transform_row[col]
-        
-    # Invert the y-axis and z-axis to convert from ARKit to Open3D coordinate system.
-    # convert_matrix = np.diag([1, -1, -1, 1])
-    # transform = convert_matrix @ transform @ convert_matrix
-
-    print(f"Loaded transform matrix from {transform_path} for trial '{row_trial_name}':\n{transform}")
-    return transform
-
-def read_intrinsics(dir_path: str, row_trial_name: str) -> np.ndarray:
-    """
-    Reads the camera intrinsics matrix from a text file.
-    """
-    if not os.path.exists(dir_path):
-        raise FileNotFoundError(f"Main dir not found: {dir_path}")
-
-    intrinsics_path = os.path.join(dir_path, f"camera_matrix.csv")
-    if not os.path.exists(intrinsics_path):
-        raise FileNotFoundError(f"Intrinsics file not found: {intrinsics_path}")
-    
-    # intrinsics = pd.read_csv(intrinsics_path, header=None)
-    intrinsics = np.loadtxt(intrinsics_path, delimiter=',')
-
-    print(f"Loaded intrinsics matrix from {intrinsics_path} for trial '{row_trial_name}':\n{intrinsics}")
-    return intrinsics
-
-def read_main_data(dir_path: str, row_trial_name: str) -> tuple[Image.Image, o3d.geometry.TriangleMesh, pd.DataFrame, pd.DataFrame]:
-    """
-    Reads the RGB image and mesh file (PLY format) from the main trial directory.
-    """
-    row_path = os.path.join(dir_path, row_trial_name)
-    if not os.path.exists(row_path):
-        raise FileNotFoundError(f"Trial dir not found: {row_path}")
-    
-    img_dir_path = os.path.join(row_path, "rgb")
-    mesh_dir_path = os.path.join(row_path, "mesh")
-
-    img = read_rgb(img_dir_path, row_trial_name)
-    mesh = read_mesh(mesh_dir_path, row_trial_name)
-    transform = read_transform(row_path, row_trial_name)
-    intrinsics = read_intrinsics(row_path, row_trial_name)
-
-    return img, mesh, transform, intrinsics
+    return corners, ids
 
 if __name__=="__main__":
     DATASET_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "dataset", "lab_controlled", "experiment_4"))
-    ROW_LABEL = "0-0-0-0-b"
+    ROW_LABEL = "0-3-3-0-b"
     
     MAIN_PATH = os.path.join(DATASET_PATH, "main")
     PCD_PATH = os.path.join(DATASET_PATH, "pcd")
     
     img, mesh, transform, intrinsics = read_main_data(MAIN_PATH, ROW_LABEL)
+    # mesh.compute_vertex_normals()
     pcd = read_pcd(PCD_PATH, ROW_LABEL)
-    pcd, adjusted_transform = align_pcd(pcd, transform)
+    # pcd, adjusted_transform = align_pcd(pcd, transform)
     
     frame_mesh = o3d.geometry.TriangleMesh.create_coordinate_frame().scale(0.1, np.zeros(3))
     reference_frame_mesh = o3d.geometry.TriangleMesh.create_coordinate_frame().scale(0.2, np.zeros(3))
 
-    o3d.visualization.draw_geometries([pcd, mesh, frame_mesh.transform(adjusted_transform), reference_frame_mesh])
+    # o3d.visualization.draw_geometries([pcd, mesh, frame_mesh.transform(adjusted_transform), reference_frame_mesh])
+
+    april_corners, april_ids = detect_apriltags(img)
+    
+    if april_ids is None: exit("No AprilTags detected.")
+    april_centroids = [np.mean(corner[0], axis=0) for corner in april_corners]
+    
+    # Mark the centroids in the mesh using intrinsics and transform
+    april_centroids_3d = []
+    april_meshes = []
+    for i, corner in enumerate(april_corners):
+        tag_id = str(april_ids[i][0])
+        
+        centroid_2d = np.mean(corner[0], axis=0)
+        x_2d, y_2d = int(centroid_2d[0]), int(centroid_2d[1])
+        
+        depth = 1.0  # Assume a nominal depth of 1 meter
+        fx, fy = intrinsics[0, 0], intrinsics[1, 1]
+        cx, cy = intrinsics[0, 2], intrinsics[1, 2]
+        
+        x_3d = (x_2d - cx) * depth / fx
+        y_3d = (y_2d - cy) * depth / fy
+        z_3d = depth
+        
+        point_camera = np.array([x_3d, y_3d, z_3d, 1.0])
+        point_world = transform @ point_camera
+        
+        tag_mesh = o3d.geometry.TriangleMesh.create_sphere(radius=0.5)
+        tag_mesh.paint_uniform_color([1, 0, 0])
+        tag_mesh.translate(point_world[:3])
+        april_meshes.append(tag_mesh)
+        print(f"AprilTag ID {tag_id} at 2D pixel ({x_2d}, {y_2d}) corresponds to 3D point {point_world[:3]}")
+        
+    o3d.visualization.draw_geometries([mesh, *april_meshes, frame_mesh.transform(transform), reference_frame_mesh])
---
+++ pcd_and_mesh/lab_db/utils/read_utils.py
@@ -0,0 +1,136 @@
+import os
+import sys
+import glob
+import numpy as np
+import pandas as pd
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+import open3d as o3d
+
+TRANSFORM_COLUMNS_TO_MATRIX_MAP = {
+    "rxx": (0, 0),
+    "rxy": (1, 0),
+    "rxz": (2, 0),
+    "ryx": (0, 1),
+    "ryy": (1, 1),
+    "ryz": (2, 1),
+    "rzx": (0, 2),
+    "rzy": (1, 2),
+    "rzz": (2, 2),
+    "x": (0, 3),
+    "y": (1, 3),
+    "z": (2, 3)
+}
+
+def read_pcd(dir_path: str, row_trial_name: str) -> o3d.geometry.PointCloud:
+    """
+    Read a point cloud file (PCD format) and return an Open3D PointCloud object.
+    """
+    if not os.path.exists(dir_path):
+        raise FileNotFoundError(f"PCD dir not found: {dir_path}")
+
+    pcd = o3d.io.read_point_cloud(os.path.join(dir_path, f"{row_trial_name}.ply"))
+    if pcd.is_empty():
+        raise ValueError(f"Loaded point cloud is empty: {row_trial_name}")
+
+    print(f"Loaded point cloud from {os.path.join(dir_path, f'{row_trial_name}.ply')} with {len(pcd.points)} points for trial '{row_trial_name}'")
+    return pcd
+
+def read_rgb(dir_path: str, row_trial_name: str) -> Image.Image:
+    """
+    Reads the RGB image from the given directory.
+    """
+    if not os.path.exists(dir_path):
+        raise FileNotFoundError(f"Main dir not found: {dir_path}")
+
+    # Currently, the rgb image can have any name, but it is placed alone in a folder as a png image.
+    img_files = glob.glob(os.path.join(dir_path, "*.png"))
+    if not img_files:
+        raise FileNotFoundError(f"No PNG image found in dir: {dir_path}")
+    img_path = img_files[0]
+
+    img = Image.open(img_path)
+    print(f"Loaded RGB image from {img_path} with size {img.size} for trial '{row_trial_name}'")
+    return img
+
+def read_mesh(dir_path: str, row_trial_name: str) -> o3d.geometry.TriangleMesh:
+    """
+    Reads the mesh file (PLY format) from the given directory.
+    """
+    if not os.path.exists(dir_path):
+        raise FileNotFoundError(f"Main dir not found: {dir_path}")
+
+    # Currently, the mesh file can have any name, but it is placed alone in a folder as a ply file.
+    mesh_files = glob.glob(os.path.join(dir_path, "*.ply"))
+    if not mesh_files:
+        raise FileNotFoundError(f"No PLY mesh file found in dir: {dir_path}")
+    mesh_path = mesh_files[0]
+
+    mesh = o3d.io.read_triangle_mesh(mesh_path)
+    if mesh.is_empty():
+        raise ValueError(f"Loaded mesh is empty: {mesh_path}")
+
+    print(f"Loaded mesh from {mesh_path} with {len(mesh.triangles)} triangles for trial '{row_trial_name}'")
+    return mesh
+
+def read_transform(dir_path: str, row_trial_name: str) -> np.ndarray:
+    """
+    Reads the camera-to-world transformation matrix from a text file.
+    """
+    if not os.path.exists(dir_path):
+        raise FileNotFoundError(f"Main dir not found: {dir_path}")
+
+    transform_path = os.path.join(dir_path, f"camera_transform.csv")
+    if not os.path.exists(transform_path):
+        raise FileNotFoundError(f"Transform file not found: {transform_path}")
+    
+    transform_df = pd.read_csv(transform_path, delimiter=', ')
+    # Will later convert to required format.
+    transform_row = transform_df.iloc[0].to_dict()
+    transform = np.eye(4)
+    for col, (i, j) in TRANSFORM_COLUMNS_TO_MATRIX_MAP.items():
+        if col not in transform_row:
+            raise ValueError(f"Column '{col}' not found in transform file: {transform_path}")
+        transform[i, j] = transform_row[col]
+        
+    # Invert the y-axis and z-axis to convert from ARKit to Open3D coordinate system.
+    # convert_matrix = np.diag([1, -1, -1, 1])
+    # transform = convert_matrix @ transform @ convert_matrix
+
+    print(f"Loaded transform matrix from {transform_path} for trial '{row_trial_name}':\n{transform}")
+    return transform
+
+def read_intrinsics(dir_path: str, row_trial_name: str) -> np.ndarray:
+    """
+    Reads the camera intrinsics matrix from a text file.
+    """
+    if not os.path.exists(dir_path):
+        raise FileNotFoundError(f"Main dir not found: {dir_path}")
+
+    intrinsics_path = os.path.join(dir_path, f"camera_matrix.csv")
+    if not os.path.exists(intrinsics_path):
+        raise FileNotFoundError(f"Intrinsics file not found: {intrinsics_path}")
+    
+    # intrinsics = pd.read_csv(intrinsics_path, header=None)
+    intrinsics = np.loadtxt(intrinsics_path, delimiter=',')
+
+    print(f"Loaded intrinsics matrix from {intrinsics_path} for trial '{row_trial_name}':\n{intrinsics}")
+    return intrinsics
+
+def read_main_data(dir_path: str, row_trial_name: str) -> tuple[Image.Image, o3d.geometry.TriangleMesh, pd.DataFrame, pd.DataFrame]:
+    """
+    Reads the RGB image and mesh file (PLY format) from the main trial directory.
+    """
+    row_path = os.path.join(dir_path, row_trial_name)
+    if not os.path.exists(row_path):
+        raise FileNotFoundError(f"Trial dir not found: {row_path}")
+    
+    img_dir_path = os.path.join(row_path, "rgb")
+    mesh_dir_path = os.path.join(row_path, "mesh")
+
+    img = read_rgb(img_dir_path, row_trial_name)
+    mesh = read_mesh(mesh_dir_path, row_trial_name)
+    transform = read_transform(row_path, row_trial_name)
+    intrinsics = read_intrinsics(row_path, row_trial_name)
+
+    return img, mesh, transform, intrinsics

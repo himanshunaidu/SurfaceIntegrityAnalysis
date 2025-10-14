740f71e0a05172e29d893c6bcf2a20bf733d7299
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-10-14 15:50:07 -0700

    Add code for running pcd-based analysis

--- pcd_and_mesh/lab/pcd_analysis_run.py
+++ pcd_and_mesh/lab/pcd_analysis_run.py
@@ -1,5 +1,5 @@
 """
-This script analyzes a 3D mesh to check for surface integrity issues.
+This script analyzes a 3D point cloud to check for surface integrity issues.
 """
 import os
 import glob
@@ -12,59 +12,67 @@ from sklearn.cluster import DBSCAN
 import logging
 import optuna
 from optuna import Trial
+import warnings
 
-from vision_utils.mesh import check_integrity as check_mesh_integrity, IntegrityDetails as MeshIntegrityDetails
+from vision_utils.pcd import check_integrity as check_pcd_integrity, IntegrityDetails as PCDIntegrityDetails
 from utils.plane import get_plane_mesh, get_viz_with_transparency
 from utils.stats import get_array_stats
 from db.schema import AttributeSchema, ResultColumns, DatasetBuildPlan, DatasetBuildPlanOverrides, FACTORS
 from db.read import load_data_frames
+from db.update import update_data_frames, update_results_data_frame
 from db.calc import calc_integrity_issue
 
-def process_mesh_files(dataset_path: str, db_results_frame: pd.DataFrame, *, 
-                    mesh_dir: str = 'mesh_cropped', output_dir: str = 'mesh_analyzed',
-                    depth_thr: float = 0.005, near_plane_thr: float = 0.05, 
-                    min_angle_thr: float = 10.0,
-                    dbscan_eps: float = 0.05, dbscan_min_samples: int = 3,
-                    issue_area_percent_thr: float = 0.005) -> None:
+def process_pcd_files(dataset_path: str, db_main_frame: pd.DataFrame, db_results_frame: pd.DataFrame, *, 
+                    pcd_dir: str = 'pcd_cropped', output_dir: str = 'pcd_analysis',
+                    depth_thr: float = 0.004, near_plane_thr: float = 0.05, 
+                    min_angle_thr: float = 15.0,
+                    dbscan_eps: float = 0.05, dbscan_min_samples: int = 5,
+                    normal_radius: float = 0.03, normal_knn: int = 30, normal_orient_k: int = 50,
+                    boundary_radius: float = 0.05, boundary_knn: int = 30, boundary_angle_thr: float = 60.0,
+                    issue_percent_thr: float = 0.005) -> None:
     """
-    Loads all mesh files from the dataset path based on the database results frame and analyzes them for
+    Loads all point cloud files from the dataset path based on the database results frame and analyzes them for
     surface integrity issues.
-    
-    Note: The db_results_frame contains column 'trial_name' based on which the mesh files are named (with some differences).
-    Each row in db_results_frame corresponds to multiple meshes in dataset_path.
-        (Multiple meshes because of repeated trials with same parameters. We can call them subtrials.)
+
+    Note: The db_results_frame contains column 'trial_name' based on which the point cloud files are named (with some differences).
+    Each row in db_results_frame corresponds to multiple point clouds in dataset_path.
+        (Multiple point clouds because of repeated trials with same parameters. We can call them subtrials.)
+
     The subtrials are named as per the 'trial_name' column in db_results_frame, along with 'a', 'b', ... suffixes for repeated trials.
     """
-    subtrial_directories = glob.glob(os.path.join(dataset_path, "*"))
-    subtrial_directories.sort()
-    subtrial_directory_names = [os.path.basename(d) for d in subtrial_directories if os.path.isdir(d)]
-    print(f"Found {len(subtrial_directory_names)} subtrial directories in {dataset_path}")
+    point_cloud_files = glob.glob(os.path.join(dataset_path, pcd_dir, "*.ply"))
+    point_cloud_files.sort()
+    point_cloud_file_names = [os.path.basename(f) for f in point_cloud_files]
+    print(f"Found {len(point_cloud_file_names)} point cloud files in {os.path.join(dataset_path, pcd_dir)}")
+    
+    output_dir_path = os.path.join(dataset_path, output_dir)
+    os.makedirs(output_dir_path, exist_ok=True)
     
     for index, row in db_results_frame.iterrows():
         trial_name = row[AttributeSchema.Columns.TRIAL_NAME.value]
+        
         num_issues = 0
-        row_subtrials = [d for d in subtrial_directory_names if d.startswith(trial_name)]
-        if not row_subtrials:
-            # print(f"No subdirectory found for trial_name '{trial_name}' in row index {index}")
+        row_pcds = [f for f in point_cloud_file_names if f.startswith(trial_name)]
+        if not row_pcds:
+            # print(f"No point cloud file found for trial_name '{trial_name}' in row index {index}")
             continue
-        for sub_dir_name in row_subtrials:
-            # Setup mesh analysis for this subdirectory
-            sub_dir_path = os.path.join(dataset_path, sub_dir_name)
-            mesh_path = os.path.join(sub_dir_path, mesh_dir)
-            mesh_files = glob.glob(os.path.join(mesh_path, "*.ply"))
-            if not mesh_files:
-                print(f"No mesh files found in {mesh_path} for trial_name '{trial_name}' in row index {index}")
-                continue
-            mesh_file = mesh_files[0]  # Assuming one .ply file per subtrial
+        
+        for pcd_file in row_pcds:
+            pcd_file_path = os.path.join(dataset_path, pcd_dir, pcd_file)
             
-            analyzed_mesh, has_issues, mesh_integrity_details = check_mesh_integrity(
-                mesh_file, depth_thr=depth_thr, near_plane_thr=near_plane_thr, min_angle_thr=min_angle_thr,
-                dbscan_eps=dbscan_eps, dbscan_min_samples=dbscan_min_samples,
-                issue_area_percent_thr=issue_area_percent_thr)
+            analyzed_pcd, has_issues, pcd_integrity_details = check_pcd_integrity(
+                pcd_file_path, depth_thr=depth_thr, near_plane_thr=near_plane_thr,
+                min_angle_thr=min_angle_thr, dbscan_eps=dbscan_eps, dbscan_min_samples=dbscan_min_samples,
+                normal_radius=normal_radius, normal_knn=normal_knn, normal_orient_k=normal_orient_k,
+                boundary_radius=boundary_radius, boundary_knn=boundary_knn, boundary_angle_thr=boundary_angle_thr,
+                issue_percent_thr=issue_percent_thr
+            )
+            
+            output_pcd_file_path = os.path.join(output_dir_path, pcd_file)
             
             if has_issues: num_issues += 1
-        db_results_frame.at[index, ResultColumns.POLYGON_MESH_RESULT.value] = num_issues
-        print(f"Completed processing for row index {index}, trial_name '{trial_name}'. {num_issues} subtrials with issues.")
+        db_results_frame.at[index, ResultColumns.POINT_CLOUD_RESULT.value] = num_issues
+        print(f"Completed processing for row index {index}, trial_name '{trial_name}': point_cloud_result = {num_issues}")
 
 def process_results(dataset_dfs: list[tuple[pd.DataFrame, pd.DataFrame]]) -> pd.DataFrame:
     """
@@ -77,11 +85,10 @@ def process_results(dataset_dfs: list[tuple[pd.DataFrame, pd.DataFrame]]) -> pd.
     SURFACE_HEIGHT_DIFFERENCE_COL = AttributeSchema.Columns.SURFACE_HEIGHT_DIFFERENCE.value
     BOARD_PLACEMENT_COL = AttributeSchema.Columns.BOARD_PLACEMENT.value
     
-    POLYGON_MESH_RESULT_COL = ResultColumns.POLYGON_MESH_RESULT.value
-    POLYGON_MESH_NUMBER_COL = ResultColumns.POLYGON_MESH_NUMBER.value
+    POINT_CLOUD_RESULT_COL = ResultColumns.POINT_CLOUD_RESULT.value
+    POINT_CLOUD_NUMBER_COL = ResultColumns.POINT_CLOUD_NUMBER.value
     
-    # Combine all datasets into a single DataFrame for analysis
-    combined_df = pd.DataFrame(columns=[GAP_WIDTH_COL, GAP_DEPTH_COL, SURFACE_HEIGHT_DIFFERENCE_COL, BOARD_PLACEMENT_COL, POLYGON_MESH_RESULT_COL, POLYGON_MESH_NUMBER_COL])
+    combined_df = pd.DataFrame(columns=[GAP_WIDTH_COL, GAP_DEPTH_COL, SURFACE_HEIGHT_DIFFERENCE_COL, BOARD_PLACEMENT_COL, POINT_CLOUD_RESULT_COL, POINT_CLOUD_NUMBER_COL])
     for df_main, df_results in dataset_dfs:
         for row in df_main.itertuples(index=False):
             trial_name = getattr(row, AttributeSchema.Columns.TRIAL_NAME.value)
@@ -94,8 +101,8 @@ def process_results(dataset_dfs: list[tuple[pd.DataFrame, pd.DataFrame]]) -> pd.
                 GAP_DEPTH_COL: getattr(row, GAP_DEPTH_COL),
                 SURFACE_HEIGHT_DIFFERENCE_COL: getattr(row, SURFACE_HEIGHT_DIFFERENCE_COL),
                 BOARD_PLACEMENT_COL: getattr(row, BOARD_PLACEMENT_COL),
-                POLYGON_MESH_RESULT_COL: result_row[POLYGON_MESH_RESULT_COL] if not pd.isna(result_row[POLYGON_MESH_RESULT_COL]) else 0,
-                POLYGON_MESH_NUMBER_COL: result_row[POLYGON_MESH_NUMBER_COL] if not pd.isna(result_row[POLYGON_MESH_NUMBER_COL]) else 0
+                POINT_CLOUD_RESULT_COL: result_row[POINT_CLOUD_RESULT_COL] if not pd.isna(result_row[POINT_CLOUD_RESULT_COL]) else 0,
+                POINT_CLOUD_NUMBER_COL: result_row[POINT_CLOUD_NUMBER_COL] if not pd.isna(result_row[POINT_CLOUD_NUMBER_COL]) else 0
             }
             # Check if exact row already exists
             existing_row = combined_df[
@@ -106,10 +113,12 @@ def process_results(dataset_dfs: list[tuple[pd.DataFrame, pd.DataFrame]]) -> pd.
             ]
             if not existing_row.empty:
                 idx = existing_row.index[0]
-                combined_df.at[idx, POLYGON_MESH_RESULT_COL] += combined_row[POLYGON_MESH_RESULT_COL]
-                combined_df.at[idx, POLYGON_MESH_NUMBER_COL] += combined_row[POLYGON_MESH_NUMBER_COL]
+                combined_df.at[idx, POINT_CLOUD_RESULT_COL] += combined_row[POINT_CLOUD_RESULT_COL]
+                combined_df.at[idx, POINT_CLOUD_NUMBER_COL] += combined_row[POINT_CLOUD_NUMBER_COL]
             else:
-                combined_df = pd.concat([combined_df, pd.DataFrame([combined_row])], ignore_index=True)
+                combined_row_df = pd.DataFrame([combined_row])
+                combined_df = pd.concat([combined_df, combined_row_df], ignore_index=True)
+    
     return combined_df
 
 def loss(combined_df: pd.DataFrame) -> tuple[float, float, float]:
@@ -118,9 +127,9 @@ def loss(combined_df: pd.DataFrame) -> tuple[float, float, float]:
     This loss is based on a kind of cross-entropy between actual and ideal results.
     
     Also returns total values, false positive rate and false negative rate.
-    """    
-    POLYGON_MESH_RESULT_COL = ResultColumns.POLYGON_MESH_RESULT.value
-    POLYGON_MESH_NUMBER_COL = ResultColumns.POLYGON_MESH_NUMBER.value
+    """
+    POINT_CLOUD_RESULT_COL = ResultColumns.POINT_CLOUD_RESULT.value
+    POINT_CLOUD_NUMBER_COL = ResultColumns.POINT_CLOUD_NUMBER.value
     
     total = 0
     false_total, false_positive = 0, 0
@@ -130,11 +139,11 @@ def loss(combined_df: pd.DataFrame) -> tuple[float, float, float]:
     fpr, fnr = 0.0, 0.0
     
     for index, row in enumerate(combined_df.itertuples(index=False)):        
-        y_total = getattr(row, POLYGON_MESH_NUMBER_COL)
+        y_total = getattr(row, POINT_CLOUD_NUMBER_COL)
         y_true = calc_integrity_issue(combined_df, index) * y_total
         if y_total == 0:
             continue
-        y_pred = getattr(row, POLYGON_MESH_RESULT_COL)
+        y_pred = getattr(row, POINT_CLOUD_RESULT_COL)
         
         p_true = y_true / y_total
         p_pred = y_pred / y_total
@@ -149,13 +158,12 @@ def loss(combined_df: pd.DataFrame) -> tuple[float, float, float]:
         else:
             true_total += y_total
             false_negative += (y_total - y_pred)
-        
+            
     fpr = (false_positive / false_total) if false_total > 0 else 0.0
     fnr = (false_negative / true_total) if true_total > 0 else 0.0
     return loss_value, total, fpr, fnr
-    
 
-def objective(trial: Trial):
+def objective(trial: Trial = None):
     depth_thr = trial.suggest_categorical("depth_thr", [0.001, 0.005, 0.01, 0.02, 0.05])
     near_plane_thr = trial.suggest_categorical("near_plane_thr", [0.05])
     min_angle_thr = trial.suggest_discrete_uniform("min_angle_thr", 2.5, 10, 2.5)
@@ -174,15 +182,15 @@ def objective(trial: Trial):
         DATASET_PATH = os.path.join(DATASET_MAIN_PATH, dataset)
         logging.info(f"Starting trial for dataset: {DATASET_PATH}")
         db_main_frame, db_results_frame = load_data_frames(DATASET_PATH)
-        dataset_mesh_path = os.path.join(DATASET_PATH, "main")
-        process_mesh_files(
-            dataset_mesh_path, db_results_frame,
+        dataset_pcd_path = DATASET_PATH #os.path.join(DATASET_PATH, "main")
+        process_pcd_files(
+            dataset_pcd_path, db_main_frame, db_results_frame,
             depth_thr=depth_thr, near_plane_thr=near_plane_thr, min_angle_thr=min_angle_thr,
             dbscan_eps=dbscan_eps, dbscan_min_samples=dbscan_min_samples,
             issue_area_percent_thr=issue_area_percent_thr
-        )
-        num_rows_with_issues = db_results_frame[db_results_frame[ResultColumns.POLYGON_MESH_RESULT.value] > 0].shape[0]
-        logging.info(f"Trial completed. Number of rows with polygon mesh issues: {num_rows_with_issues}\n")
+        )   
+        num_rows_with_issues = db_results_frame[db_results_frame[ResultColumns.POINT_CLOUD_RESULT.value] > 0].shape[0]
+        logging.info(f"Trial completed. Number of rows with point cloud issues: {num_rows_with_issues}\n")
         
         dataset_dfs.append((db_main_frame, db_results_frame))
     
@@ -194,14 +202,14 @@ def objective(trial: Trial):
 if __name__=="__main__":
     DATASET_MAIN_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "dataset", "lab_controlled"))
     logging.basicConfig(
-        filename=os.path.join(DATASET_MAIN_PATH, "mesh_analysis_run.log"),
+        filename=os.path.join(DATASET_MAIN_PATH, "pcd_analysis_run.log"),
         filemode='w',
         format='%(asctime)s - %(levelname)s - %(message)s',
         level=logging.INFO
     )
     
-    study = optuna.create_study(direction="minimize", study_name="mesh_integrity_optimization")
-    study.optimize(objective, n_trials=500, timeout=7200)  # 2 hours timeout
+    study = optuna.create_study(direction="minimize", study_name="pcd_integrity_optimization")
+    study.optimize(objective, n_trials=1, timeout=7200)  # 2 hours timeout
     
     # Print the best trial
     best_trial = study.best_trial
